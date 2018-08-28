685940d1f4303e747fd2f26a85b79d4a9e647bff
cuongpp9 <cuongpp9@gmail.com>
2018-08-28 09:10:21 +0700

    fix author

--- src/index.js
+++ src/index.js
@@ -9,48 +9,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { shuffle, sample } from 'underscore';
 import AddAuthorForm from './Layouts/AddAuthorForm';
 import DemoExamples from './DemoExamples';
-
-
-const authors = [
-    {
-        name: 'Mark Twain',
-        imageUrl: '/assets/MarkTwain.jpg',
-        imageSource: 'Wikimedia Commons',
-        books: ['The Adventures of Huckleberry Finn']
-    },
-    {
-        name: 'Joseph Conrad',
-        imageUrl: '/assets/JosephConrad.png',
-        imageSource: 'Wikimedia Commons',
-        books: ['Heart of Darkness']
-    },
-    {
-        name: 'J.K. Rowling',
-        imageUrl: '/assets/J._K._Rowling.jpg',
-        imageSource: 'Wikimedia Commons',
-        imageAttribution: 'Daniel Ogren',
-        books: ['Harry Potter and the Sorcerers Stone']
-    },
-    {
-        name: 'Stephen King',
-        imageUrl: '/assets/Stephen_King.jpg',
-        imageSource: 'Wikimedia Commons',
-        imageAttribution: 'Pinguino',
-        books: ['The Shining', 'IT']
-    },
-    {
-        name: 'Charles Dickens',
-        imageUrl: '/assets/charlesDickens.jpg',
-        imageSource: 'Wikimedia Commons',
-        books: ['David Copperfield', 'A Tale of Two Cities']
-    },
-    {
-        name: 'William Shakespeare',
-        imageUrl: '/assets/williamShakespeare.jpg',
-        imageSource: 'Wikimedia Commons',
-        books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
-    }
-];
+import authors from './utilities/Authors';
 
 function getTurnData(authors) {
     //console.log("authors", authors);
@@ -80,6 +39,7 @@ function reducer(
     action) {
       switch (action.type) {
         case 'ANSWER_SELECTED':
+        console.log("action",action)
           const isCorrect = state.turnData.author.books.some((book) => book === action.answer);
           return Object.assign(
             {}, 
---
+++ src/utilities/Authors.js
@@ -0,0 +1,42 @@
+const authors = [
+    {
+        name: 'Mark Twain',
+        imageUrl: '/assets/MarkTwain.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['The Adventures of Huckleberry Finn']
+    },
+    {
+        name: 'Joseph Conrad',
+        imageUrl: '/assets/JosephConrad.png',
+        imageSource: 'Wikimedia Commons',
+        books: ['Heart of Darkness']
+    },
+    {
+        name: 'J.K. Rowling',
+        imageUrl: '/assets/J._K._Rowling.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Daniel Ogren',
+        books: ['Harry Potter and the Sorcerers Stone']
+    },
+    {
+        name: 'Stephen King',
+        imageUrl: '/assets/Stephen_King.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Pinguino',
+        books: ['The Shining', 'IT']
+    },
+    {
+        name: 'Charles Dickens',
+        imageUrl: '/assets/charlesDickens.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['David Copperfield', 'A Tale of Two Cities']
+    },
+    {
+        name: 'William Shakespeare',
+        imageUrl: '/assets/williamShakespeare.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+    }
+];
+
+export default authors;
